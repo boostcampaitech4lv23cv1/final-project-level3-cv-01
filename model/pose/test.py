9d6d8203e2cf7fce4358d27999b42d2a6cdef46c
Yoonjae Cho <nicolasc0714@gmail.com>
2023-01-14 11:27:32 +0000

    feat: Add time calculation of hand presence
    when the hand is present, you get a message of how much time is elapsed
    from the beginning of the video. Also created the corresponding python
    file as test.py
    
    Related to: #2

---
+++ model/pose/test.py
@@ -0,0 +1,134 @@
+import mediapipe as mp  # Import mediapipe
+import cv2  # Import opencv
+
+import numpy as np
+import time
+from datetime import datetime
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+mp_holistic = mp.solutions.holistic
+mp_drawing_styles = mp.solutions.drawing_styles
+
+
+def calculate_angle(a, b):
+    a = np.array(a)
+    b = np.array(b)
+
+    radians = np.arctan2(b[1] - a[1], b[0] - a[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    return angle
+
+
+def run():
+    cap = cv2.VideoCapture("./mhchoi.mp4")
+    anomaly = {"shoulder": [], "hand": []}
+
+    # Initiate holistic model
+    with mp_holistic.Holistic(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as holistic:
+        start_time = datetime.now()
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # Recolor Feed
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            # Make Detections
+            results = holistic.process(image)
+
+            # Recolor image back to BGR for rendering
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # Right hand
+            mp_drawing.draw_landmarks(
+                image,
+                results.right_hand_landmarks,
+                mp_holistic.HAND_CONNECTIONS,
+                mp_drawing.DrawingSpec(
+                    color=(80, 22, 10), thickness=2, circle_radius=4
+                ),
+                mp_drawing.DrawingSpec(
+                    color=(80, 44, 121), thickness=2, circle_radius=2
+                ),
+            )
+
+            # Left Hand
+            mp_drawing.draw_landmarks(
+                image,
+                results.left_hand_landmarks,
+                mp_holistic.HAND_CONNECTIONS,
+                mp_drawing.DrawingSpec(
+                    color=(121, 22, 76), thickness=2, circle_radius=4
+                ),
+                mp_drawing.DrawingSpec(
+                    color=(121, 44, 250), thickness=2, circle_radius=2
+                ),
+            )
+
+            # Pose Detections
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_holistic.POSE_CONNECTIONS,
+                mp_drawing.DrawingSpec(
+                    color=(245, 117, 66), thickness=2, circle_radius=4
+                ),
+                mp_drawing.DrawingSpec(
+                    color=(245, 66, 230), thickness=2, circle_radius=2
+                ),
+            )
+            # Export coordinates
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                # Get shoulder coordinates
+                left_shoulder = [
+                    landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].y,
+                ]
+                right_shoulder = [
+                    landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].x,
+                    landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].y,
+                ]
+
+                # Calculate shoulder angle
+                angle = calculate_angle(left_shoulder, right_shoulder)
+                if angle <= 170:  # 기준 정해야함
+                    anomaly["shoulder"].append(time.time())
+                else:
+                    shoulder_seconds = anomaly["shoulder"][-1] - anomaly["shoulder"][0]
+                    if shoulder_seconds >= 1e-3:
+                        print(f"{shoulder_seconds:.3f}초 동안 자세가 좋지 않았습니다.")
+                    anomaly["shoulder"] = []
+
+                if results.left_hand_landmarks or results.right_hand_landmarks:
+                    current_time = datetime.now()
+                    target_time = current_time - start_time
+                    print(f"{target_time.total_seconds():.3f}초에 손이 나왔습니다.")
+
+            except:
+                pass
+
+            cv2.imshow("Video Feed", image)
+
+            if cv2.waitKey(10) & 0xFF == ord("q"):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+    cv2.waitKey(1)
+    cv2.waitKey(1)
+    cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    run()
