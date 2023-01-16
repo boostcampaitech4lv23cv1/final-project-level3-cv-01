0b8355609ec30fb2fe90a85af6a682fddf615ad8
MyeongheonChoi <audgjs99@gmail.com>
2023-01-16 11:48:04 +0900

    feat : streamlit final demo version - Baek's version + Choi's version - recording 에서 제한 시간 설정 및 영상 녹화 후 영상 확인 기능 추가 - streamlit run streamlit/HEY-I.py 로 실행 가능

--- streamlit/pages/recording.py
+++ streamlit/pages/recording.py
@@ -1,74 +1,110 @@
-import sys
+import os
 import cv2
+import sys
+import time
+import tempfile
+from pytz import timezone
+from datetime import datetime
 import streamlit as st
 
-# stop = st.button('Stop Recording Video')
-# stop = st.radio(
-#         "Recording Start or Stop",
-#         ["Record Start", "Record Stop"],
-#         horizontal=True,
-#     )
-st.markdown(
-    '''
-    # 면접 자세 분석을 위해 녹화할게요!
-    ### 준비가 되면 녹화 버튼을 눌러주세요
-    '''
-)
-start = st.button('Start Recording')
-stframe = st.empty()
+# st.session_state.start_recording = False
+# st.session_state.end_recording = False
 
-# if stop == "Record Start":
-if start:
-    st.markdown(
-        '''
-        #### 질문 : 자기 소개를 1분 안에 해주세요
-        '''
-    )
-    stop = st.button('Stop Recording')
-    while not stop:
+# print(st.session_state)
 
-        with st.spinner('get ready for camera'):
+# Basic App Scaffolding
+st.title('HEY-I')
+st.subheader('면접 영상을 녹화하세요')
+st.markdown('##### 선택한 시간이 지나거나 End Recording 버튼을 누르면 녹화가 종료됩니다.')
 
-            cap = cv2.VideoCapture(0)
+# Create Sidebar
+st.sidebar.title('Settings')
 
-            if not cap.isOpened():
-                print("Camera open failed!")
-                st.write("Camera open failed!")
-                sys.exit()
+## Get Video
+temp_file = tempfile.NamedTemporaryFile(delete=False)
 
-            w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            fps = cap.get(cv2.CAP_PROP_FPS)
+number = st.sidebar.number_input('분 입력',1,10)
+start_recording = st.sidebar.button('Start Recordinging')
+# start_recording = st.sidebar.button('Start Recordinging', key='start_recording')
 
-            fourcc = cv2.VideoWriter_fourcc(*'X264') # *'DIVX' == 'D', 'I', 'V', 'X'
-            delay = round(1000 / fps)
+if start_recording:
+    # print(st.session_state.start_recording)
+    st.markdown('**질문** : 1분 자기 소개를 해주세요')
+    stframe = st.empty()
+    with st.spinner('Get Ready for Camera'):
+        video = cv2.VideoCapture(0)
 
-            out = cv2.VideoWriter('output.mp4', fourcc, fps, (w, h))
+        #Load Web Camera
+        if not (video.isOpened()):
+            print("File isn't opend!!")
 
-            # if not out.isOpened():
-            #     print('File open failed!')
-            #     cap.release()
-            #     sys.exit()
+        #Set Video File Property
+        w = round(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        h = round(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        framecount = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        #video.set(cv2.CAP_PROP_FPS, 10) # fps 설정
+        fps = video.get(cv2.CAP_PROP_FPS)
+        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        # delay = 6
+        print('fps:',fps)
+        print('framecount:',framecount)
 
-        with st.spinner('Recording!'):
+        #Save Video
+        if not os.path.exists('./db'):
+            os.makedirs('./db')
+        start_time = datetime.now(timezone("Asia/Seoul")).strftime("_%y%m%d_%H%M%S")
+        video_dir = f'./db/output{start_time}.mp4'
+        st.session_state.video_dir = video_dir
+        out = cv2.VideoWriter(video_dir, fourcc, fps, (w,h))
+        if not (out.isOpened()):
+            print("File isn't opend!!")
+            video.release()
+            sys.exit()
 
-            while True:
-                ret, frame = cap.read()
+    end_recording = st.sidebar.button('End Recordinging')
+    # end_recording = st.sidebar.button('End Recordinging', key='end_recording')
 
-                if not ret:
-                    break
+    #Load frame and Save it
+    start = time.time()
+    timer = st.sidebar.empty()
+    num_frames = 0
+    while video.isOpened() and start_recording and not end_recording:
+        ret, frame = video.read()
 
-                # inversed = ~frame
+        sec = round(time.time() - start)
+        timer.metric("Countdown", f"{sec//60:02d}:{sec%60:02d}")
 
+        if ret and sec//60<number:
+            num_frames += 1
+
+            stframe.image(frame,channels='BGR', use_column_width=True)
+
+            if start_recording:
                 out.write(frame)
+            
+            cv2.waitKey(1)
 
-                # cv2.imshow('frame', frame)
-                stframe.image(frame, channels = 'BGR')
-                # cv2.imshow('inversed', inversed)
+        else:
+            print("ret is false")
+            break
+    print('num frames:',num_frames)
+    print()
 
-                # if cv2.waitKey(delay) == 27:
-                #     break
+    video.release()
+    out.release()
+    cv2.destroyAllWindows()
 
-# cap.release()
-# out.release()
-# cv2.destroyAllWindows()
+# print(st.session_state)
+
+# if end_recording and os.path.exists(st.session_state.video_dir):
+#     st.write(f'{video_dir}에 면접 영상이 저장되었습니다. 수고하셨습니다!')
+
+if 'video_dir' in st.session_state.keys():
+    if os.path.exists(st.session_state.video_dir):
+        # print(st.session_state.video_dir)
+        video_file = open(st.session_state.video_dir, 'rb')
+        video_bytes = video_file.read()
+        st.write('가장 최근 녹화된 영상입니다. 이 영상으로 업로드 할 것인지 결정해주세요')
+        check = st.button('Check Video')
+        if check:
+            st.video(video_bytes)
