b07f3b0ef279b167897e4f398edf111ed5498a16
MyeongheonChoi <audgjs99@gmail.com>
2023-01-19 16:16:10 +0900

    feat : make result template

--- streamlit/pages/result.py
+++ streamlit/pages/result.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 
 BACKEND_FACE = 'http://127.0.0.1:8000/face_emotion'
-
+st.set_page_config(layout='wide')
 st.title('HEY-I')
 
 if 'confirm_video' in st.session_state.keys():
@@ -20,21 +20,40 @@ if 'confirm_video' in st.session_state.keys():
         
         inference = st.button('Inference')
         if inference:
-            VIDEO_PATH = st.session_state.confirm_video
-            SAVED_DIR = os.path.join(os.path.splitext(st.session_state.confirm_video)[0], 'frames')
+            # VIDEO_PATH = st.session_state.confirm_video
+            # SAVED_DIR = os.path.join(os.path.splitext(st.session_state.confirm_video)[0], 'frames')
 
-            input_json = {
-                'VIDEO_PATH' : VIDEO_PATH,
-                'SAVED_DIR' : SAVED_DIR
-            }
+            # input_json = {
+            #     'VIDEO_PATH' : VIDEO_PATH,
+            #     'SAVED_DIR' : SAVED_DIR
+            # }
 
-            with st.spinner('inferencing...'):
-                r = requests.post(
-                    BACKEND_FACE, json=input_json
-                )
+            # with st.spinner('inferencing...'):
+            #     r = requests.post(
+            #         BACKEND_FACE, json=input_json
+            #     )
             
-            result = pd.read_json(r.text, orient = 'records')
-            st.dataframe(result)
+            # result = pd.read_json(r.text, orient = 'records')
+
+            with st.expander('영상 분석 결과 확인'):
+                video_file = open('db/output_230119_155322.mp4', 'rb')
+                video_bytes = video_file.read()
+                st.write('영상 분석 결과입니다.')
+                st.video(video_bytes)
+
+            tab1, tab2, tab3 = st.tabs(['Emotion', 'Pose', 'Eye'])
+            
+            with tab1:
+                st.header('Emotion')
+                st.subheader('니 얼굴 표정 이렇다 임마 표정 좀 풀어라')
+
+            with tab2:
+                st.header('Pose')
+                st.subheader('니 자세가 이렇다 삐딱하이 에픽하이')
+            
+            with tab3:
+                st.header('Eye')
+                st.subheader('동태눈깔 꼬라보노 보노보노')
 
     else:
         st.subheader('면접 영상이 제대로 저장되지 않았습니다. 다시 면접 영상을 녹화해주세요.')
