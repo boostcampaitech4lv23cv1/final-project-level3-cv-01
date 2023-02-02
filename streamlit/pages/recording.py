da382223274a98c492bb9b5cbbdda841b10f2e42
quasar529 <quasar0529@gmail.com>
2023-02-03 01:50:58 +0900

    feat: WebRTC
    - linux에서 웹캠을 열어야 하기 때문에 VideoCapture 대신 WebRTC 사용
    - 영상 저장 형식 flv라 미리보기가 안돼 녹화 후 webm으로 변환 (영상의 길이의
    절반 정도 소요됨)
    - prepare.py 페이지 변경 고려
      - 아예 없애거나, 다른 모습으로 바꿀 예정.
      - 그 전까지는 Cloud run으로 배포 시 prepare 제외하고 사용.

--- streamlit/pages/recording.py
+++ streamlit/pages/recording.py
@@ -1,16 +1,16 @@
 import os
-import cv2
 import sys
+import cv2
 import time
 import tempfile
+import uuid
+from pathlib import Path
 from pytz import timezone
 from datetime import datetime
 import streamlit as st
 from google.cloud import storage
 
-
 sys.path.append(os.getcwd())
-
 import av
 from aiortc.contrib.media import MediaRecorder
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
@@ -20,6 +20,7 @@ from FastAPI.utils import upload_video, download_video
 # Basic App Scaffolding
 st.title("HEY-I")
 st.subheader("면접 영상을 녹화하세요")
+st.write("❗ 카메라 접근 권한을 승인해주세요 ❗")
 st.markdown("##### 선택한 시간이 지나거나 End Recording 버튼을 누르면 녹화가 종료됩니다.")
 
 # Create Sidebar
@@ -67,114 +68,48 @@ webrtc_streamer(
     video_frame_callback=video_frame_callback,
     in_recorder_factory=in_recorder_factory,
 )
-# st.session_state.start_recording = False
-# st.session_state.end_recording = False
+###########################################################
+st.session_state.video_dir = (
+    f"./{st.session_state.name}_{st.session_state.num}/{prefix}/recording.webm"
+)
+with st.spinner("✔ 확인됐습니다. 변환 중입니다..."):
+    start = time.process_time()
+    cap = cv2.VideoCapture(in_file)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*"vp80")
 
-# print(st.session_state)
-
-# Basic App Scaffolding
-# st.title("HEY-I")
-# st.subheader("면접 영상을 녹화하세요")
-# st.markdown("##### 선택한 시간이 지나거나 End Recording 버튼을 누르면 녹화가 종료됩니다.")
-
-# # Create Sidebar
-# st.sidebar.title("Settings")
-
-# ## Get Video
-# temp_file = tempfile.NamedTemporaryFile(delete=False)
-
-# number = st.sidebar.number_input("분 입력", 1, 10)
-# start_recording = st.sidebar.button("Start Recording")
-# # start_recording = st.sidebar.button('Start Recordinging', key='start_recording')
-
-# if start_recording:
-#     # print(st.session_state.start_recording)
-#     st.markdown("**질문** : 1분 자기 소개를 해주세요")
-#     stframe = st.empty()
-#     with st.spinner("Get Ready for Camera"):
-#         # video = cv2.VideoCapture('/opt/ml/TEST_VIDEO/ka.mp4')
-#         video = cv2.VideoCapture(0)
-#         # Load Web Camera
-#         if not (video.isOpened()):
-#             print("File isn't opened!!")
-
-#         # Set Video File Property
-#         w = round(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-#         h = round(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#         framecount = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-#         # video.set(cv2.CAP_PROP_FPS, 10) # fps 설정
-#         fps = video.get(cv2.CAP_PROP_FPS)
-#         fourcc = cv2.VideoWriter_fourcc(*"vp80")
-#         # delay = 6
-#         print("fps:", fps)
-#         print("framecount:", framecount)
-
-#         # Save Video
-#         start_time = datetime.now(timezone("Asia/Seoul")).strftime("%y%m%d_%H%M%S")
-#         # if not os.path.exists(f"./{st.session_state.name}_{st.session_state.num}"):
-#         #     os.makedirs(f"./{st.session_state.name}_{st.session_state.num}")
-#         if not os.path.exists(f"./{st.session_state.name}_{st.session_state.num}/{start_time}"):
-#             os.makedirs(f"./{st.session_state.name}_{st.session_state.num}/{start_time}")
-
-#         video_dir = f"./{st.session_state.name}_{st.session_state.num}/{start_time}/recording.webm"
-#         st.session_state.video_dir = video_dir
-#         out = cv2.VideoWriter(video_dir, fourcc, fps / 4, (w, h))
-#         if not (out.isOpened()):
-#             print("File isn't opened!!")
-#             video.release()
-#             sys.exit()
-
-#     end_recording = st.sidebar.button("End Recording")
-
-#     # Load frame and Save it
-#     start = time.time()
-#     timer = st.sidebar.empty()
-#     num_frames = 0
-#     while video.isOpened() and start_recording and not end_recording:
-#         ret, frame = video.read()
-
-#         sec = round(time.time() - start)
-#         timer.metric("Countdown", f"{sec//60:02d}:{sec%60:02d}")
-
-#         if ret and sec // 60 < number:
-#             num_frames += 1
-
-#             stframe.image(frame, channels="BGR", use_column_width=True)
-
-#             if start_recording:
-#                 out.write(frame)
-
-#             cv2.waitKey(1)
-
-#         else:
-#             print("ret is false")
-#             break
-#     print("num frames:", num_frames)
-#     print()
-
-#     video.release()
-#     out.release()
-
-#     cv2.destroyAllWindows()
-
-
-# if end_recording and os.path.exists(st.session_state.video_dir):
-#     st.write(f'{video_dir}에 면접 영상이 저장되었습니다. 수고하셨습니다!')
+    out = cv2.VideoWriter(
+        st.session_state.video_dir,
+        fourcc,
+        fps,
+        (width, height),
+    )
+    while True:
+        ret, frame = cap.read()
+        if not ret:  # 새로운 프레임을 못받아 왔을 때 braek
+            break
+        out.write(frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    end = time.process_time()
+    print(f"Convert Complete: {st.session_state.video_dir} on {end-start}")
 
 if "video_dir" in st.session_state.keys():
     if os.path.exists(st.session_state.video_dir):
-        # print(st.session_state.video_dir)
         video_file = open(st.session_state.video_dir, "rb")
         video_bytes = video_file.read()
-        st.write("가장 최근 녹화된 영상을 확인하시겠습니까?")
-        check = st.checkbox("Check Video")
-        if check:
-            with st.expander("가장 최근 녹화된 영상입니다. 이 영상으로 업로드 할 것인지 결정해주세요"):
-                st.video(video_bytes)
-
-                # 분석할 영상 결정
-                st.write("이 영상으로 분석을 진행할까요?")
-                confirm = st.button("Comfirm")
-                if confirm:
-                    st.write("분석할 영상이 확인 되었습니다. Result 에서 결과를 확인하세요.")
-                    st.session_state.confirm_video = st.session_state.video_dir
+        st.write("녹화된 영상을 확인하시겠습니까?")
+        with st.expander("가장 최근 녹화된 영상입니다. 이 영상을 분석 할 지 결정해주세요"):
+            st.video(video_bytes)
+            # 분석할 영상 결정
+            st.write("이 영상으로 분석을 진행할까요?")
+            confirm = st.button("Comfirm")
+            if confirm:
+                st.write("분석할 영상이 확인 되었습니다. Result 에서 결과를 확인하세요.")
+                st.session_state.confirm_video = st.session_state.video_dir
